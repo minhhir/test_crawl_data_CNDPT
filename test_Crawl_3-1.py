ec073a032456692592646b96ca23d727c7dec04f
minhhir <139110895+minhhir@users.noreply.github.com>
2026-01-19 14:11:16 +0700

    Remove existing dataset file before new scrape
    Add logic to remove existing dataset before scraping.

--- test_Crawl_3-1.py
+++ test_Crawl_3-1.py
@@ -101,4 +101,8 @@ def scrape_data(base_url, target_count=300):#target_count: Số bài tự dộng
 
 
 if __name__ == "__main__":
+    #mỗi lần cào thì sẽ tạo dataset mới đè lên cái cũ
+    import os
+    if os.path.exists("dataset.csv"):
+        os.remove("dataset.csv")
     scrape_data("https://vnexpress.net/thoi-su", target_count=350) #target_count: Số bài muốn cào
